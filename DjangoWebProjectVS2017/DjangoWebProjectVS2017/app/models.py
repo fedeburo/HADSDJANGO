2523ff7d02c0e28ac30c9ad441db06d40a58f75e
Fede Buroni <fedeburo@gmail.com>
2019-05-16 13:01:08 +0200

    commit

---
+++ DjangoWebProjectVS2017/DjangoWebProjectVS2017/app/models.py
@@ -0,0 +1,22 @@
+"""
+Definition of models.
+"""
+
+from django.db import models
+from django.db.models.fields import CharField
+
+# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    tema= models.CharField(default =' ', max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    correcto = models.BooleanField(default=False)
+
+class User(models.Model):
+    email = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=200)
